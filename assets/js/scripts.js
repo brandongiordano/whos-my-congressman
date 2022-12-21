244052698f7cdd5c45e54b88e4a720ab718b1342
brandongiordano <bgiordano9917@aol.com>
2022-12-20 20:58:39 -0500

    populate politicians name

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -6,5 +6,39 @@ fetch(url)
         return response.json();
     })
     .then(function (data) {
+<<<<<<< HEAD
         console.log(data);
     })
+=======
+        wikiAPI(data.officials[2].name);
+    })
+
+}
+
+
+function wikiAPI( name){
+    
+    var apiURL = "https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch="+name+"&format=json&origin=*";
+
+
+    fetch(apiURL)
+    .then(function(response){
+            if(response.ok){
+                response.json().then(function(data){
+                    console.log(data);
+                    console.log(data.query.search[0].snippet);
+                    displayInfo(data);
+                })
+            }
+            else{
+                console.log("failed")
+            }
+    })
+}
+
+function displayInfo(data) {
+   $("#congressman-name").html(data.query.search[0].title);
+   console.log("test", data);
+}
+getName();
+>>>>>>> 9512305 (populate politicians name)
